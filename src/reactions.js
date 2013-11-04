5e9a66992c9ece257c65b55ce89c78e58563ea76
Valeri Bogdanov <valeribogdanov@gmail.com>
2013-11-04 17:34:59 +0200

    waterfall and tests

--- src/reactions.js
+++ src/reactions.js
@@ -5,18 +5,59 @@
   } else if (typeof exports === 'object') {
     module.exports = factory();
   } else {
-    root.reactions = factory();
+    root.Reactions = factory();
   }
 }(this, function () {
   'use strict';
+  var EMPTY_FUNCTION = function () {};
 
+  var Reactions = {};
 
-  var result = {
-    'fn': {},
-    'make': {}
+  /** 
+   * Creates a 'done' function.
+   * @argument defaultDone - the done function to delegate to.
+   * @argument success - [optional] - the success function to delegate to.
+   * A done function can be invoked only once, receives an error and callback
+   * in case of error invokes the defaultDone passing the error
+   * in case of success invokes sucess passing the data or 
+   * the defaultDone if no success is present passing false as first argument and
+   * data as the second
+   */
+  Reactions.done = function (defaultDone, success) {
+    var invoked = false;
+    if (typeof success !== 'function')
+      success = function (data) { defaultDone(false, data); };
+    
+    return function (error, data) {
+      if (invoked)
+        throw new Error('callback functions should be invoked only once');
+      invoked = true;
+      Reactions.invokeLater(function () {
+        if (error)
+          defaultDone(error);
+        else 
+          success(data);
+      });
+    };
   };
 
-  result.fn.series = function(reactions, context, done) {
+  /**
+   * Invokes the passed function later in the event queue. 
+   * Essentially cross-platform implementation of process.nextTick
+   */
+  //TODO: use a better implementation for the browsers, as setTimeout(fn, 0) is quite slow
+  Reactions.invokeLater = (process && process.nextTick && typeof process.nextTick === 'function')?
+      process.nextTick:
+      function (fn) { setTimeout(fn, 0); };
+
+
+  Reactions.fn = {};
+  /**
+   * Invoke all reactions in order, waiting for the previous to complete. 
+   * All reactions and done are invoked with the original context.
+   * Executions stops on first error and done(error) is invoked
+   */
+  Reactions.fn.series = function(reactions, context, done) {
     var results = [];
     var toRun = -1;
     function next(error, value) {
@@ -27,7 +68,7 @@
       results[toRun] = value;
       toRun += 1;
       if (toRun < reactions.length) {
-        reactions[toRun].call(null, context, next);
+        reactions[toRun].call(null, context, Reactions.done(next));
       } else {
         done(null, context);
       }
@@ -35,11 +76,18 @@
     next();
   };
 
-  result.fn.parallel = function(reactions, context, done) {
+  /**
+   * Execution of the reactions is started in the order in which they appear. 
+   * done is invoked when all reactions complete successfully or on the first received error
+   */
+  Reactions.fn.parallel = function(reactions, context, done) {
     var countback = reactions.length + 1;
-
+    var finished = false;
     function next(error, data) {
+      if (finished) return; //callback has been invoked already
+
       if (error) {
+        finished = true;
         done(error);
         return;
       }
@@ -49,29 +97,51 @@
     }
 
     for (var i = 0; i < reactions.length; i ++) {
-      reactions[i].call(null, context, next);
+      reactions[i].call(null, context, Reactions.done(next));
     }
 
     next();
   };
 
-  //attack the make functions
-  for (var key in result.fn) {
-    var dofn = result.fn[key];
-    result.make[key] = function () {
-      var _key = key;
-      var _dofn = dofn;
-      var argcount = dofn.length - 2;
-      var args = Array.prototype.slice.call(arguments, argcount);
+ /**
+  * Each reactions is invoked using the result of the previous one as context.
+  * The first is fed the original context. Done is invoked with the result of the last one.
+  * Execution is stopped on error and done(error) is invoked.
+  */
+  Reactions.fn.waterfall = function (reactions, context, done) {
+    if (reactions.length > 0) {
+      var head = reactions[0];
+      var tail = reactions.slice(1);
+      head.call(null, context, Reactions.done(done, function (data) {
+        Reactions.fn.waterfall(tail, data, done);
+      }));
+    } else {
+      done(false, context);
+    }
+  }
+
+  function constructMakeFn(fn) {
+    return function () {
+      var argcount = fn.length - 2;
+      var args = Array.prototype.slice.call(arguments, 0, argcount); //all arguments except context and done
       return function (world, done) {
-        console.log('invoked make.' + _key);
         args = args.slice(0);
         args.push(world);
         args.push(done);
-        _dofn.apply(null, args);
+        fn.apply(null, args);
       };
-    };
+    }; 
   }
 
-  return result;
+ /**
+  * Reactions.make.XXX is expecting all the arguments of Reactions.fn.XXX except context and done and
+  * creates a function(context, done)
+  */
+  Reactions.make = {};
+  //attach the make functions
+  for (var key in Reactions.fn) {
+    Reactions.make[key] = constructMakeFn(Reactions.fn[key]);
+  }
+
+  return Reactions;
 }));
--- test/reactions.spec.js
+++ test/reactions.spec.js
@@ -40,7 +40,7 @@ describe('reactions', function (argument) {
 
       reactor(context, function (error, data) {
         expect(data).toBe(context);
-        expect(error).toBeNull();
+        expect(error).toBeFalsy();
         [fn1, fn2, fn3].forEach(function (fn) {
           expect(fn).toHaveBeenCalledWith(context, jasmine.any(Function));
         });
@@ -54,7 +54,7 @@ describe('reactions', function (argument) {
 
       reactor(context, function (error, data) {
         expect(data).toBeUndefined();
-        expect(error).not.toBeNull();
+        expect(error).not.toBeFalsy();
         expect(fn1).toHaveBeenCalledWith(context, jasmine.any(Function));
         expect(fnErr).toHaveBeenCalledWith(context, jasmine.any(Function));
         expect(fn3).not.toHaveBeenCalled();
@@ -68,7 +68,7 @@ describe('reactions', function (argument) {
 
       reactor(context, function (error, data) {
         expect(data).toBe(context);
-        expect(error).toBeNull();
+        expect(error).toBeFalsy();
         expect(fn1).toHaveBeenCalledWith(context, jasmine.any(Function));
         next();
       });
@@ -81,7 +81,7 @@ describe('reactions', function (argument) {
 
       reactor(context, function (error, data) {
         expect(data).toBe(context);
-        expect(error).toBeNull();
+        expect(error).toBeFalsy();
         next();
       });
     });
@@ -99,7 +99,7 @@ describe('reactions', function (argument) {
 
       reactor(context, function (error, data) {
         expect(data).toBe(context);
-        expect(error).toBeNull();
+        expect(error).toBeFalsy();
         expect(fn2).toHaveBeenCalledWith(context, jasmine.any(Function));
         next();
       });
@@ -109,7 +109,164 @@ describe('reactions', function (argument) {
 
   describe('parallel', function (argument) {
     
+    it('is a function', function () {
+      expect(reactions.make.parallel).toEqual(jasmine.any(Function));
+    });
+
+    it('invokes the reactions passing the original context', function (next) {
+      var context = { test: true };
+      var reactor = reactions.make.parallel([fn1, fn2, fn3]);
+
+      reactor(context, function (error, data) {
+        expect(data).toBe(context);
+        expect(error).toBeFalsy();
+        [fn1, fn2, fn3].forEach(function (fn) {
+          expect(fn).toHaveBeenCalledWith(context, jasmine.any(Function));
+        });
+        next();
+      });
+    });
+
+    it('passes the first exception to done', function (next) {
+      var context = { test: true };
+      var reactor = reactions.make.parallel([fn1, fnErr, fn3]); 
+
+      reactor(context, function (error, data) {
+        expect(data).toBeUndefined();
+        expect(error).toBeDefined();
+        expect(fn1).toHaveBeenCalledWith(context, jasmine.any(Function));
+        expect(fnErr).toHaveBeenCalledWith(context, jasmine.any(Function));
+        expect(fn3).toHaveBeenCalledWith(context, jasmine.any(Function));
+        next();
+      });
+    });
+
+    it('works with only one function', function (next) {
+      var context = { test: true };
+      var reactor = reactions.make.parallel([fn1]); 
+
+      reactor(context, function (error, data) {
+        expect(data).toBe(context);
+        expect(error).toBeFalsy();
+        expect(fn1).toHaveBeenCalledWith(context, jasmine.any(Function));
+        next();
+      });
+    });
+
+
+    it('works with no functions', function (next) {
+      var context = { test: true };
+      var reactor = reactions.make.parallel([]); 
+
+      reactor(context, function (error, data) {
+        expect(data).toBe(context);
+        expect(error).toBeFalsy();
+        next();
+      });
+    });
+
+
+    it('invokes them in order', function (next) {
+      var context = { test: true };
+      var delayedFirst = function (world, done) {
+        setTimeout(function () {
+          expect(fn2).toHaveBeenCalled();
+          done(null, world);
+        }, 100);
+      };
+      var reactor = reactions.make.parallel([delayedFirst, fn2]);
+
+      reactor(context, function (error, data) {
+        expect(data).toBe(context);
+        expect(error).toBeFalsy();
+        expect(fn2).toHaveBeenCalledWith(context, jasmine.any(Function));
+        next();
+      });
+    });
+
+  });
+
+
+  describe('waterfall', function (argument) {
+    var fn1, fn2, fn3;
+    function addOne(context, done) {
+      done(false, context + 1);
+    }
+    beforeEach(function () {
+      fn1 = jasmine.createSpy('1').andCallFake(addOne);
+      fn2 = jasmine.createSpy('2').andCallFake(addOne);
+      fn3 = jasmine.createSpy('3').andCallFake(addOne);
+    });
     
+    it('is a function', function () {
+      expect(reactions.make.waterfall).toEqual(jasmine.any(Function));
+    });
+
+    it('invokes the reactions passing the result of the previous as context', function (next) {
+      var reactor = reactions.make.waterfall([fn1, fn2, fn3]);
+
+      reactor(0, function (error, data) {
+        expect(data).toBe(3);
+        expect(error).toBeFalsy();
+        [fn1, fn2, fn3].forEach(function (fn, index) {
+          expect(fn).toHaveBeenCalledWith(index, jasmine.any(Function));
+        });
+        next();
+      });
+    });
+
+    it('passes the first exception to done', function (next) {
+      var reactor = reactions.make.waterfall([fn1, fnErr, fn3]); 
+
+      reactor(0, function (error, data) {
+        expect(data).toBeUndefined();
+        expect(error).toBeDefined();
+        expect(fn1).toHaveBeenCalledWith(0, jasmine.any(Function));
+        expect(fnErr).toHaveBeenCalledWith(1, jasmine.any(Function));
+        expect(fn3).not.toHaveBeenCalled();
+        next();
+      });
+    });
+
+    it('works with only one function', function (next) {
+      var reactor = reactions.make.waterfall([fn1]); 
+
+      reactor(0, function (error, data) {
+        expect(data).toBe(1);
+        expect(error).toBeFalsy();
+        expect(fn1).toHaveBeenCalledWith(0, jasmine.any(Function));
+        next();
+      });
+    });
+
+
+    it('works with no functions', function (next) {
+      var reactor = reactions.make.waterfall([]); 
+
+      reactor(0, function (error, data) {
+        expect(data).toBe(0);
+        expect(error).toBeFalsy();
+        next();
+      });
+    });
+
+
+    it('invokes them in order', function (next) {
+      var delayedFirst = function (world, done) {
+        setTimeout(function () {
+          expect(fn2).not.toHaveBeenCalled();
+          done(false, world + 1);
+        }, 100);
+      };
+      var reactor = reactions.make.waterfall([delayedFirst, fn2]);
+
+      reactor(40, function (error, data) {
+        expect(data).toBe(42);
+        expect(error).toBeFalsy();
+        expect(fn2).toHaveBeenCalledWith(41, jasmine.any(Function));
+        next();
+      });
+    });
 
   });
 
